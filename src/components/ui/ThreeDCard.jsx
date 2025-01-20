ec43c695b1c20933f0191f9cbe94055932d0cd1f
DMC <dmchakma47@gmail.com>
2025-01-20 16:49:06 +0530

    updated resume

--- src/components/ui/ThreeDCard.jsx
+++ src/components/ui/ThreeDCard.jsx
@@ -25,7 +25,7 @@ export function ThreeDCard() {
         <div className="flex justify-center items-center mt-10">
           <CardItem translateZ={20} className="px-4 py-2">
             <a
-              href="/port-resume2.pdf"
+              href="/port-resume.pdf"
               download
               target="_blank"
               rel="noopener noreferrer"
